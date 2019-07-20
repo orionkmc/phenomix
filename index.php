b3e54afeb0bd647061fff116f92ef7e745b68fae
kuai-mare carrasquero <orionkmc@gmail.com>
2019-07-20 13:17:07 -0400

    ordenando css

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
 	<link rel="stylesheet" href="assets/dist/swiper/dist/css/swiper.css">
 	<link rel="stylesheet" href="assets/dist/slick-1.8.1/slick/slick.css">
 	<link rel="stylesheet" href="assets/dist/animate/animate.css">
+
+
+	<link rel="stylesheet" href="assets/css/font.css">
 	<link rel="stylesheet" href="styles/main.css">
 
 
@@ -131,11 +134,12 @@
 </main>
 
 <script>!function(e,a,t,n,g,c){e.GoogleAnalyticsObject=n,e.ga||(e.ga=function(){(e.ga.q=e.ga.q||[]).push(arguments)}),e.ga.l=+new Date,g=a.createElement(t),c=a.getElementsByTagName(t)[0],g.src="https://www.google-analytics.com/analytics.js",c.parentNode.insertBefore(g,c)}(window,document,"script","ga"),ga("create","UA-XXXXX-X"),ga("send","pageview");</script>
-
+<!-- nuevas -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+<!-- desconocidas -->
 <script src="scripts/vendor.js"></script>
 <script src="scripts/main.js"></script>
 
