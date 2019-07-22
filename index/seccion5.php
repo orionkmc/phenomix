f79b7a26bbe57ce141d4fb6f32a5689d2e056b24
Jonwilm <jparedes84@gmail.com>
2019-07-21 20:09:43 -0400

    Ajustes Seccion 5 - Mobile

--- index/seccion5.php
+++ index/seccion5.php
@@ -1,5 +1,5 @@
 <div class="swiper-slide d-sm-none" data-hash="why-it-works-2">
-  <section id="why-it-works-2" class="container-fluid">
+  <section id="why-it-works-2" class="container-fluid d-flex align-items-center">
       <div class="row align-items-center">
           <div class="col-12 col-lg-6 h-50 mt-5">
               <img src="assets/images/maximizing-graphic-2.png" data-toggle="appear" data-appear-class="fadeIn slow delay-1s" class="appear img-fluid mt-5">
