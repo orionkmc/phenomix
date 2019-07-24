55bfbd1b680b366a1f8b861d3bef04941c645f91
kuai-mare carrasquero <orionkmc@gmail.com>
2019-07-24 14:43:34 -0400

    licences

--- index/seccion8.php
+++ index/seccion8.php
@@ -1,36 +1,81 @@
 <div class="swiper-slide" data-hash="licences">
-    <section id="licences" class="px-3 px-lg-5 d-lg-flex">
-        <div class="container">
-            <div class="row licences-affiliations h-auto">
-                <div class="col-12 col-lg-3 licences mt-5 mt-lg-0">
-                    <div class="content p-3 text-center appear" data-appear-class="fadeInDown" data-toggle="appear">
-                        <h1 class="appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">Licences & Accreditations</h1>
-                        <p class="appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">Clinical laboratory improvement amendments (clia) certificate of accreditation</p>
-                        <p class="appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">24D2109855</p>
-                        <p class="appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">10/25/2020</p>
-                        <img src="assets/images/licence-blue.png" class="img-fluid appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">
-                    </div>
-                </div>
-                <div class="col-12 col-lg-9 affiliations d-none d-lg-block">
-                    <div class="content p-3 text-center appear" data-appear-class="fadeInDown" data-toggle="appear">
-                        <h1 class="appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">Affiliations, Partners and Resources</h1>
-                        <img class="img-fluid m-4" src="assets/images/licences/1.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/2.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/3.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/4.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/5.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/6.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/7.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/8.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/9.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/10.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/11.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/12.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/13.png" alt="">
-                        <img class="img-fluid m-4" src="assets/images/licences/14.png" alt="">
-                    </div>
-                </div>
-            </div>
+  <section id="licences" class="px-3 px-lg-5 d-lg-flex">
+    <div class="container">
+      <div class="row licences-affiliations h-auto">
+        <div class="col-12 col-lg-3 licences">
+          <div class="content p-3 text-center appear" data-appear-class="fadeInDown" data-toggle="appear">
+            <h1 class="appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">Licences & Accreditations</h1>
+            <p class="appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">Clinical laboratory improvement amendments (clia) certificate of accreditation</p>
+            <p class="appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">24D2109855</p>
+            <p class="appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">10/25/2020</p>
+            <img src="assets/images/licence-blue.png" class="img-fluid appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">
+          </div>
         </div>
-    </section>
+        <div class="col-12 col-lg-9 affiliations d-none d-lg-block">
+          <div class="content p-3 text-center appear" data-appear-class="fadeInDown" data-toggle="appear">
+
+            <h1 class="appear" data-appear-class="fadeInDown delay-500" data-toggle="appear">Affiliations, Partners and Resources</h1>
+            <div class="img-container">
+              <div class="img"><img class="img-fluid" src="assets/images/licences/1.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/2.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/3.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/4.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/5.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/6.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/7.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/8.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/9.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/10.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/11.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/12.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/13.png" alt=""></div>
+              <div class="img"><img class="img-fluid" src="assets/images/licences/14.png" alt=""></div>
+            </div>
+            <!-- <div class="img-affiliations">
+              <div><img src="assets/images/licences/1.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/2.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/3.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/4.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/5.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/6.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/7.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/8.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/9.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/10.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/11.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/12.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/13.png" alt=""></div>
+            </div>
+            <div class="img-affiliations">
+              <div><img src="assets/images/licences/14.png" alt=""></div>
+            </div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </div>
