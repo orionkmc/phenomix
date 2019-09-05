80424b6362821aaf2ad64867780af3e6a586ce2d
Jonwilm <jparedes84@gmail.com>
2019-09-05 15:05:20 -0400

    Preload About

--- about.php
+++ about.php
@@ -29,34 +29,11 @@
 <!--[if IE]>
 <p class="browserupgrade">You are using an outdated browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
-<div class="placa"></div>
-<div class="GPDR-white"></div>
-<div class="modal fade" id="modalGPDR" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-body">
-        <h4>We use cookies</h4>
-        <p>We use cookies and other tracking technologies to improve your browsing experience on our website, to show you personalized content and targeted ads, to analyze our website traffic, and to understand where our visitors are coming from. By browsing our website, you consent to our use of cookies and other tracking technologies.</p>
-        <br>
-        <a href="#">Check our Privacy Policy</a>
-        <br>
-        <br>
-        <a href="#" class="btn-bio float-right" data-dismiss="modal">Accept</a>
-      </div>
-    </div>
-  </div>
-</div>
+<?php include 'index/cookies.php'; ?>
 
 <!-- preloader start -->
-<div class="landscape"></div>
-<div class="preloader-bg"></div>
-<div id="preloader">
-  <div id="preloader-status">
-    <div class="preloader-position loader">
-      <span></span>
-    </div>
-  </div>
-</div><!-- preloader end -->
+<?php include 'index/preloader.php'; ?>
+<!-- preloader end -->
 
 <main>
   <?php include 'index/nav_2.php'; ?>
